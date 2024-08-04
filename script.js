9e8ba86850c8c50b19666dbae5deae6fce3afb7a
Durotan <50570415+JanFojtik21@users.noreply.github.com>
2024-08-04 19:58:09 +0200

    added buttons, preparing for future

---
+++ script.js
@@ -0,0 +1,7 @@
+const homeButton = document.getElementById('home-button');
+const moviesButton = document.getElementById('movies-button');
+const myList = document.getElementById('myList-button');
+const newsButton = document.getElementById('news-button');
+
+
+
