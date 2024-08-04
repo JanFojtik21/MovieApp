70b1a1c8709ab8b2ade345f1b80c73dd56fda37e
Durotan <50570415+JanFojtik21@users.noreply.github.com>
2024-08-04 20:28:10 +0200

    pridani traces, funkcionalita tlacitek provizorni, treba dodelat wise implementaci ostatniho

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const homeButton = document.getElementById('home-button');
-const moviesButton = document.getElementById('movies-button');
-const myList = document.getElementById('myList-button');
-const newsButton = document.getElementById('news-button');
+document.addEventListener('DOMContentLoaded', () => {
+    const homeButton = document.getElementById('home-button');
+    const moviesButton = document.getElementById('movies-button');
+    const myList = document.getElementById('myList-button');
+    const newsButton = document.getElementById('news-button');
+    const getMeBack = document.getElementById('return-to-home');
 
+    if (homeButton) {
+        homeButton.addEventListener("click", event => {
+            window.location.replace("homePage.html");
+        });
+    }
 
-
+    if (getMeBack) {
+        getMeBack.addEventListener('click', event => {
+            window.location.replace("index.html");
+        });
+    }
+});
